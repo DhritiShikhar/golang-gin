22a1007cf8fc1d06fbe9b8827000d57c73fd5b3b
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2018-05-22 17:28:54 +0530

    Corrects variable name

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default() // This creates a gin router with default middleware
 
-	r.GET("/", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world",
 		})
 	})
-
+	router.Run()
 }
